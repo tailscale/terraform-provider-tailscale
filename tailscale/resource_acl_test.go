5ee8e631230efb08af2d65c3d6e006bf63bec963
David Bond <davidsbond@users.noreply.github.com>
2021-04-28 23:50:32 +0100

    Add support for TagOwners and Tests in ACL configuration (#12)
    Fixes #11
    
    Adds support for both the `Tests` and `TagOwners` fields in ACL configuration. These were previously not included
    in the request to the ACL endpoint. Updates the acceptance test to include a full ACL and adds a test to ensure
    JSON marshalling of the ACL works as expected.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- tailscale/resource_acl_test.go
+++ tailscale/resource_acl_test.go
@@ -16,6 +16,32 @@ const testACL = `
 					"users": ["*"],
 					"ports": ["*:*"]
 				}
+			],
+			"tagowners": {
+				"tag:example": [
+					"group:example"
+				]
+			},
+			"groups": {
+				"group:example": [
+					"user1@example.com",
+					"user2@example.com"
+				]
+			},
+			"hosts": {
+				"example-host-1": "100.100.100.100",
+				"example-host-2": "100.100.101.100/24"
+			},
+			"tests": [
+				{
+					"user": "user1@example.com",
+					"allow": ["example-host-1:22", "example-host-2:80"],
+					"deny": ["exapmle-host-2:100"]
+				},
+				{
+					"user": "user2@example.com",
+					"allow": ["100.60.3.4:22"]
+				}
 			]
 		}
 		EOF
