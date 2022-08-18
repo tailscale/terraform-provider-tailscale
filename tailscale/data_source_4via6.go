a173b1ba171a2b4c386f9c8fcf8e09003c052afa
Tim Gossett <tim@mrgossett.com>
2022-08-18 14:56:11 -0400

    improve documentation

--- tailscale/data_source_4via6.go
+++ tailscale/data_source_4via6.go
@@ -12,7 +12,7 @@ import (
 
 func dataSource4Via6() *schema.Resource {
 	return &schema.Resource{
-		Description: "Calculate a 4via6 address",
+		Description: "The 4via6 data source is calculates an IPv6 prefix for a given site ID and IPv4 CIDR. See Tailscale documentation for [4via6 subnets](https://tailscale.com/kb/1201/4via6-subnets/) for more details.",
 		ReadContext: dataSource4Via6Read,
 		Schema: map[string]*schema.Schema{
 			"site": {
@@ -40,7 +40,7 @@ func dataSource4Via6Read(ctx context.Context, d *schema.ResourceData, m interfac
 	site := uint32(d.Get("site").(int))
 	cidr, err := netaddr.ParseIPPrefix(d.Get("cidr").(string))
 	if err != nil {
-		return diagnosticsError(err, "invalid CIDR")
+		return diagnosticsError(err, "Provided CIDR is invalid")
 	}
 
 	via, err := tsaddr.MapVia(site, cidr)
