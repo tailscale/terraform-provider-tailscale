261b336e437c3ca12e814a63fc7fbfe5bb4a7c23
Anton Tolchanov <anton@tailscale.com>
2022-11-25 19:23:25 +0000

    Do not overwrite ACL file that has been changed manually
    Overwriting non-default ACL contents is confusing, so now users will be
    prompted to explicitly import their existing ACL into Terraform if they
    want to manage it there. This will make overwriting existing contents
    harder, since after importing Terraform will show a sensible diff in
    `terraform apply`.
    
    Fixes #182.

--- tailscale/resource_acl.go
+++ tailscale/resource_acl.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/go-cty/cty"
@@ -22,7 +23,12 @@ func resourceACL() *schema.Resource {
 		ReadContext:   resourceACLRead,
 		CreateContext: resourceACLCreate,
 		UpdateContext: resourceACLUpdate,
-		DeleteContext: resourceACLDelete,
+		// Each tailnet always has an associated ACL file, so deleting a resource will
+		// only remove it from Terraform state, leaving ACL contents intact.
+		Delete: schema.Noop,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
 		Schema: map[string]*schema.Schema{
 			"acl": {
 				Type:             schema.TypeString,
@@ -82,7 +88,16 @@ func resourceACLCreate(ctx context.Context, d *schema.ResourceData, m interface{
 		return diagnosticsError(err, "Failed to unmarshal ACL")
 	}
 
-	if err := client.SetACL(ctx, acl); err != nil {
+	// Setting the `ts-default` ETag will make this operation succeed only if
+	// ACL contents has never been changed from its default value.
+	if err := client.SetACL(ctx, acl, tailscale.WithETag("ts-default")); err != nil {
+		if strings.HasSuffix(err.Error(), "(412)") {
+			err = fmt.Errorf(
+				"! You seem to be trying to overwrite a non-default ACL with a tailscale_acl resource.\n"+
+					"Before doing this, please import your existing ACL into Terraform state using:\n"+
+					" terraform import $(this_resource) acl\n"+
+					"(got error %q)", err)
+		}
 		return diagnosticsError(err, "Failed to set ACL")
 	}
 
@@ -101,26 +116,6 @@ func resourceACLUpdate(ctx context.Context, d *schema.ResourceData, m interface{
 	acl, err := unmarshalACL(aclStr)
 	if err != nil {
 		return diagnosticsError(err, "Failed to unmarshal ACL")
-	}
-
-	if err := client.SetACL(ctx, acl); err != nil {
-		return diagnosticsError(err, "Failed to set ACL")
-	}
-
-	return nil
-}
-
-func resourceACLDelete(ctx context.Context, _ *schema.ResourceData, m interface{}) diag.Diagnostics {
-	client := m.(*tailscale.Client)
-
-	acl := tailscale.ACL{
-		ACLs: []tailscale.ACLEntry{
-			{
-				Action: "accept",
-				Users:  []string{"*"},
-				Ports:  []string{"*:*"},
-			},
-		},
 	}
 
 	if err := client.SetACL(ctx, acl); err != nil {
