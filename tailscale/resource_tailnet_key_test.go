bc2f999b66777c78e5c70c3f9a788630b221f5c6
Cameron Stokes <cameron@cameronstokes.com>
2023-09-12 10:36:06 -0700

    resource_tailnet_key: fix/add tests for expired keys

--- tailscale/resource_tailnet_key_test.go
+++ tailscale/resource_tailnet_key_test.go
@@ -3,6 +3,7 @@ package tailscale_test
 import (
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
@@ -26,14 +27,35 @@ func TestProvider_TailscaleTailnetKey(t *testing.T) {
 		PreCheck: func() {
 			testServer.ResponseCode = http.StatusOK
 			testServer.ResponseBody = tailscale.Key{
-				ID:  "test",
-				Key: "thisisatestkey",
+				ID:      "test",
+				Key:     "thisisatestkey",
+				Expires: time.Now().Add(time.Hour * 24),
 			}
 		},
 		ProviderFactories: testProviderFactories(t),
 		Steps: []resource.TestStep{
 			testResourceCreated("tailscale_tailnet_key.example_key", testTailnetKey),
 			testResourceDestroyed("tailscale_tailnet_key.example_key", testTailnetKey),
+		},
+	})
+}
+
+func TestProvider_TailscaleTailnetKeyExpired(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		IsUnitTest: true,
+		PreCheck: func() {
+			testServer.ResponseCode = http.StatusOK
+			testServer.ResponseBody = tailscale.Key{
+				ID:      "test",
+				Key:     "thisisatestkey",
+				Expires: time.Now().Add(-time.Hour * 24),
+			}
+		},
+		ProviderFactories: testProviderFactories(t),
+		Steps: []resource.TestStep{
+			testResourceCreated("tailscale_tailnet_key.example_key", testTailnetKey),
+			// expect the resource to be re-created immediately
+			testResourceCreated("tailscale_tailnet_key.example_key", testTailnetKey),
 		},
 	})
 }
