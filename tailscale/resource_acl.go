fcfdc50f3eb9cf7d2f1f774b753d7f114b769e39
David Symonds <dsymonds@gmail.com>
2024-01-28 20:32:00 +1100

    Document that ACL tests are run when creating and updating tailscale_acl resources.
    Fixes #158.
    Closes #161.

--- tailscale/resource_acl.go
+++ tailscale/resource_acl.go
@@ -17,9 +17,13 @@ import (
 	"github.com/tailscale/tailscale-client-go/tailscale"
 )
 
+const resourceACLDescription = `The acl resource allows you to configure a Tailscale ACL. See https://tailscale.com/kb/1018/acls for more information. Note that this resource will completely overwrite existing ACL contents for a given tailnet.
+
+If tests are defined in the ACL (the top-level "tests" section), ACL validation will occur before creation and update operations are applied.`
+
 func resourceACL() *schema.Resource {
 	return &schema.Resource{
-		Description:   "The acl resource allows you to configure a Tailscale ACL. See https://tailscale.com/kb/1018/acls for more information. Note that this resource will completely overwrite existing ACL contents for a given tailnet.",
+		Description:   resourceACLDescription,
 		ReadContext:   resourceACLRead,
 		CreateContext: resourceACLCreate,
 		UpdateContext: resourceACLUpdate,
