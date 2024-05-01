380cf9a7fbcabae886122f7a7a25bc478a215d56
Mario Minardi <mario@tailscale.com>
2024-05-01 13:28:53 -0600

    tailscale: fix default User-Agent header to include terraform version (#361)
    The `schema.Provider.UserAgent` helper function was previously being
    called at a point in the plugin lifecycle before the `TerraformVersion`
    for the provider was set by Terraform Core. This was resulting in the
    Terraform version information being missing from the User-Agent header.
    The call to `schema.Provider.UserAgent` has now been moved into the
    `providerConfigure` func as this is called later in the configure
    lifecycle where the Terraform version is properly set.
    
    Fixes https://github.com/tailscale/corp/issues/19630
    
    Signed-off-by: Mario Minardi <mario@tailscale.com>

--- main.go
+++ main.go
@@ -8,24 +8,10 @@ import (
 	"github.com/tailscale/terraform-provider-tailscale/tailscale"
 )
 
-// version is filled by goreleaser at build time.
-var version = "dev"
-
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return tailscale.Provider(addUserAgent)
+			return tailscale.Provider()
 		},
 	})
-}
-
-// addUserAgent adds a `user_agent` configuration key to the provider with a
-// default value based on provider version.
-func addUserAgent(p *schema.Provider) {
-	p.Schema["user_agent"] = &schema.Schema{
-		Type:        schema.TypeString,
-		Default:     p.UserAgent("terraform-provider-tailscale", version),
-		Optional:    true,
-		Description: "User-Agent header for API requests.",
-	}
 }
