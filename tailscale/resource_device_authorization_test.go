825491044f465508c3369e5dccb2b3a5a858fe96
Percy Wegmann <percy@tailscale.com>
2024-08-08 12:57:23 -0500

    tailscale: update contacts and device authorization acceptance tests to use new utilities
    With this, all existing acceptance tests follow the same pattern.
    
    Updates #cleanup
    
    Signed-off-by: Percy Wegmann <percy@tailscale.com>

--- tailscale/resource_device_authorization_test.go
+++ tailscale/resource_device_authorization_test.go
@@ -9,10 +9,13 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
-	"github.com/tailscale/terraform-provider-tailscale/tailscale"
+	tsclient "github.com/tailscale/tailscale-client-go/v2"
 )
 
-const testDeviceAuthorization = `
+func TestAccTailscaleDeviceAuthorization_Basic(t *testing.T) {
+	const resourceName = "tailscale_device_authorization.test_authorization"
+
+	const testDeviceAuthorization = `
 	data "tailscale_device" "test_device" {
 		name = "%s"
 	}
@@ -22,46 +25,8 @@ const testDeviceAuthorization = `
 		authorized = true
 	}`
 
-func TestAccTailscaleDeviceAuthorization_Basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
-		ProviderFactories: testAccProviderFactories(t),
-		CheckDestroy:      testAccCheckDeviceAuthorizationDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: generateBasicConfig(),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDeviceAuthorized("tailscale_device_authorization.test_authorization"),
-					resource.TestCheckResourceAttr("tailscale_device_authorization.test_authorization", "authorized", "true"),
-				),
-			},
-			{
-				ResourceName:      "tailscale_device_authorization.test_authorization",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-func generateBasicConfig() string {
-	return fmt.Sprintf(testDeviceAuthorization, os.Getenv("TAILSCALE_TEST_DEVICE_NAME"))
-}
-
-func testAccCheckDeviceAuthorized(resourceName string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("resource not found: %s", resourceName)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("resource has no ID set")
-		}
-
-		client := testAccProvider.Meta().(*tailscale.Clients).V2
-		// Devices are not currently deauthorized when this resource is deleted,
-		// expect that the device both exists and is still authorized.
+	checkAuthorized := func(client *tsclient.Client, rs *terraform.ResourceState) error {
+		// Check that the device both exists and is still authorized.
 		device, err := client.Devices().Get(context.Background(), rs.Primary.ID)
 		if err != nil {
 			return err
@@ -73,31 +38,26 @@ func testAccCheckDeviceAuthorized(resourceName string) resource.TestCheckFunc {
 
 		return nil
 	}
-}
 
-func testAccCheckDeviceAuthorizationDestroy(s *terraform.State) error {
-	client := testAccProvider.Meta().(*tailscale.Clients).V2
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "tailscale_device_authorization" {
-			continue
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("resource has no ID set")
-		}
-
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories(t),
 		// Devices are not currently deauthorized when this resource is deleted,
 		// expect that the device both exists and is still authorized.
-		device, err := client.Devices().Get(context.Background(), rs.Primary.ID)
-		if err != nil {
-			return err
-		}
-
-		if device.Authorized != true {
-			return fmt.Errorf("device with id %q is not authorized", rs.Primary.ID)
-		}
-	}
-
-	return nil
+		CheckDestroy: checkResourceDestroyed(resourceName, checkAuthorized),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testDeviceAuthorization, os.Getenv("TAILSCALE_TEST_DEVICE_NAME")),
+				Check: resource.ComposeTestCheckFunc(
+					checkResourceRemoteProperties(resourceName, checkAuthorized),
+					resource.TestCheckResourceAttr(resourceName, "authorized", "true"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
 }
