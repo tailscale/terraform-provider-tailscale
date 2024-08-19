4b0a238e14e9f02687b59b362a1466a71066a051
Percy Wegmann <percy@tailscale.com>
2024-08-19 14:32:05 -0500

    v2: add TailnetSettingsResource
    Supports getting and updating Tailnet settings.
    
    Updates tailscale/corp#21628
    
    Signed-off-by: Percy Wegmann <percy@tailscale.com>

---
+++ tailscale/resource_tailnet_settings_test.go
@@ -0,0 +1,105 @@
+package tailscale_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	tsclient "github.com/tailscale/tailscale-client-go/v2"
+)
+
+func TestAccTailscaleTailnetSettings(t *testing.T) {
+	const resourceName = "tailscale_tailnet_settings.test_settings"
+
+	const testTailnetSettingsCreate = `
+		resource "tailscale_tailnet_settings" "test_settings" {
+			devices_approval_on = true
+			devices_auto_updates_on = true
+			devices_key_duration_days = 5
+			users_approval_on = true
+			users_role_allowed_to_join_external_tailnet = "member"
+			posture_identity_collection_on = true
+		}`
+
+	const testTailnetSettingsUpdate = `
+		resource "tailscale_tailnet_settings" "test_settings" {
+			devices_approval_on = false
+			devices_auto_updates_on = false
+			devices_key_duration_days = 10
+			users_approval_on = false
+			users_role_allowed_to_join_external_tailnet = "admin"
+			posture_identity_collection_on = false
+		}`
+
+	checkProperties := func(expected *tsclient.TailnetSettings) func(client *tsclient.Client, rs *terraform.ResourceState) error {
+		return func(client *tsclient.Client, rs *terraform.ResourceState) error {
+			actual, err := client.TailnetSettings().Get(context.Background())
+			if err != nil {
+				return err
+			}
+
+			if diff := cmp.Diff(expected, actual); diff != "" {
+				t.Fatalf("diff found (-got, +want): %s", diff)
+			}
+
+			return nil
+		}
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testTailnetSettingsCreate,
+				Check: resource.ComposeTestCheckFunc(
+					checkResourceRemoteProperties(resourceName,
+						checkProperties(&tsclient.TailnetSettings{
+							DevicesApprovalOn:                      true,
+							DevicesAutoUpdatesOn:                   true,
+							DevicesKeyDurationDays:                 5,
+							UsersApprovalOn:                        true,
+							UsersRoleAllowedToJoinExternalTailnets: tsclient.RoleAllowedToJoinExternalTailnetsMember,
+							PostureIdentityCollectionOn:            true,
+						}),
+					),
+					resource.TestCheckResourceAttr(resourceName, "devices_approval_on", "true"),
+					resource.TestCheckResourceAttr(resourceName, "devices_auto_updates_on", "true"),
+					resource.TestCheckResourceAttr(resourceName, "devices_key_duration_days", "5"),
+					resource.TestCheckResourceAttr(resourceName, "users_approval_on", "true"),
+					resource.TestCheckResourceAttr(resourceName, "users_role_allowed_to_join_external_tailnet", "member"),
+					resource.TestCheckResourceAttr(resourceName, "posture_identity_collection_on", "true"),
+				),
+			},
+			{
+				Config: testTailnetSettingsUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					checkResourceRemoteProperties(resourceName,
+						checkProperties(&tsclient.TailnetSettings{
+							DevicesApprovalOn:                      false,
+							DevicesAutoUpdatesOn:                   false,
+							DevicesKeyDurationDays:                 10,
+							UsersApprovalOn:                        false,
+							UsersRoleAllowedToJoinExternalTailnets: tsclient.RoleAllowedToJoinExternalTailnetsAdmin,
+							PostureIdentityCollectionOn:            false,
+						}),
+					),
+					resource.TestCheckResourceAttr(resourceName, "devices_approval_on", "false"),
+					resource.TestCheckResourceAttr(resourceName, "devices_auto_updates_on", "false"),
+					resource.TestCheckResourceAttr(resourceName, "devices_key_duration_days", "10"),
+					resource.TestCheckResourceAttr(resourceName, "users_approval_on", "false"),
+					resource.TestCheckResourceAttr(resourceName, "users_role_allowed_to_join_external_tailnet", "admin"),
+					resource.TestCheckResourceAttr(resourceName, "posture_identity_collection_on", "false"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
