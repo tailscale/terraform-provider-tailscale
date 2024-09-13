a46f5005f0ed5750247448f1cc237f553cf2efc0
Mario Minardi <mario@tailscale.com>
2024-09-13 11:14:58 -0600

    {all}: check for license headers in CI (#431)
    Check license headers in CI runs with scripts/check_license_headers.sh.
    Also adjust LICENSE to reflect the current authors.
    
    Updates #cleanup
    
    Signed-off-by: Mario Minardi <mario@tailscale.com>

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Copyright (c) David Bond, Tailscale Inc, & Contributors
+// SPDX-License-Identifier: MIT
+
 //go:generate tfplugindocs
 package main
 
--- tools.go
+++ tools.go
@@ -1,3 +1,6 @@
+// Copyright (c) David Bond, Tailscale Inc, & Contributors
+// SPDX-License-Identifier: MIT
+
 //go:build tools
 
 package tools
