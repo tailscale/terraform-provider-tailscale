e790560ddfb9bee1efbf6197a27dd7ffecbdf3fb
mcoulombe <max@tailscale.com>
2025-05-23 14:02:12 -0400

    tailscale_tailnet_settings: add acl management fields
    Updates #28349
    
    Signed-off-by: mcoulombe <max@tailscale.com>

--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/tailscale/hujson v0.0.0-20221223112325-20486734a56a
 	golang.org/x/tools v0.32.0
 	tailscale.com v1.80.2
-	tailscale.com/client/tailscale/v2 v2.0.0-20250509161557-5fad10cf3a33
+	tailscale.com/client/tailscale/v2 v2.0.0-20250602205246-d51fc603f5ea
 )
 
 require github.com/pkg/errors v0.9.1
--- tailscale/resource_tailnet_settings_test.go
+++ tailscale/resource_tailnet_settings_test.go
@@ -18,6 +18,8 @@ func TestAccTailscaleTailnetSettings(t *testing.T) {
 
 	const testTailnetSettingsCreate = `
 		resource "tailscale_tailnet_settings" "test_settings" {
+			acls_externally_managed_on = true
+			acls_external_link = "https://foo.com"
 			devices_approval_on = true
 			devices_auto_updates_on = true
 			devices_key_duration_days = 5
@@ -28,12 +30,18 @@ func TestAccTailscaleTailnetSettings(t *testing.T) {
 
 	const testTailnetSettingsUpdate = `
 		resource "tailscale_tailnet_settings" "test_settings" {
+			acls_externally_managed_on = false
+			acls_external_link = "https://bar.com"
 			devices_approval_on = false
 			devices_auto_updates_on = false
 			devices_key_duration_days = 10
 			users_approval_on = false
 			users_role_allowed_to_join_external_tailnet = "admin"
 			posture_identity_collection_on = false
+		}`
+
+	const testTailnetSettingsEmpty = `
+		resource "tailscale_tailnet_settings" "test_settings" {
 		}`
 
 	checkProperties := func(expected *tailscale.TailnetSettings) func(client *tailscale.Client, rs *terraform.ResourceState) error {
@@ -60,6 +68,8 @@ func TestAccTailscaleTailnetSettings(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					checkResourceRemoteProperties(resourceName,
 						checkProperties(&tailscale.TailnetSettings{
+							ACLsExternallyManagedOn:                true,
+							ACLsExternalLink:                       "https://foo.com",
 							DevicesApprovalOn:                      true,
 							DevicesAutoUpdatesOn:                   true,
 							DevicesKeyDurationDays:                 5,
@@ -68,6 +78,8 @@ func TestAccTailscaleTailnetSettings(t *testing.T) {
 							PostureIdentityCollectionOn:            true,
 						}),
 					),
+					resource.TestCheckResourceAttr(resourceName, "acls_externally_managed_on", "true"),
+					resource.TestCheckResourceAttr(resourceName, "acls_external_link", "https://foo.com"),
 					resource.TestCheckResourceAttr(resourceName, "devices_approval_on", "true"),
 					resource.TestCheckResourceAttr(resourceName, "devices_auto_updates_on", "true"),
 					resource.TestCheckResourceAttr(resourceName, "devices_key_duration_days", "5"),
@@ -81,6 +93,8 @@ func TestAccTailscaleTailnetSettings(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					checkResourceRemoteProperties(resourceName,
 						checkProperties(&tailscale.TailnetSettings{
+							ACLsExternallyManagedOn:                false,
+							ACLsExternalLink:                       "https://bar.com",
 							DevicesApprovalOn:                      false,
 							DevicesAutoUpdatesOn:                   false,
 							DevicesKeyDurationDays:                 10,
@@ -89,6 +103,33 @@ func TestAccTailscaleTailnetSettings(t *testing.T) {
 							PostureIdentityCollectionOn:            false,
 						}),
 					),
+					resource.TestCheckResourceAttr(resourceName, "acls_externally_managed_on", "false"),
+					resource.TestCheckResourceAttr(resourceName, "acls_external_link", "https://bar.com"),
+					resource.TestCheckResourceAttr(resourceName, "devices_approval_on", "false"),
+					resource.TestCheckResourceAttr(resourceName, "devices_auto_updates_on", "false"),
+					resource.TestCheckResourceAttr(resourceName, "devices_key_duration_days", "10"),
+					resource.TestCheckResourceAttr(resourceName, "users_approval_on", "false"),
+					resource.TestCheckResourceAttr(resourceName, "users_role_allowed_to_join_external_tailnet", "admin"),
+					resource.TestCheckResourceAttr(resourceName, "posture_identity_collection_on", "false"),
+				),
+			},
+			{
+				Config: testTailnetSettingsEmpty,
+				Check: resource.ComposeTestCheckFunc(
+					checkResourceRemoteProperties(resourceName,
+						checkProperties(&tailscale.TailnetSettings{
+							ACLsExternallyManagedOn:                false,
+							ACLsExternalLink:                       "https://bar.com",
+							DevicesApprovalOn:                      false,
+							DevicesAutoUpdatesOn:                   false,
+							DevicesKeyDurationDays:                 10,
+							UsersApprovalOn:                        false,
+							UsersRoleAllowedToJoinExternalTailnets: tailscale.RoleAllowedToJoinExternalTailnetsAdmin,
+							PostureIdentityCollectionOn:            false,
+						}),
+					),
+					resource.TestCheckResourceAttr(resourceName, "acls_externally_managed_on", "false"),
+					resource.TestCheckResourceAttr(resourceName, "acls_external_link", "https://bar.com"),
 					resource.TestCheckResourceAttr(resourceName, "devices_approval_on", "false"),
 					resource.TestCheckResourceAttr(resourceName, "devices_auto_updates_on", "false"),
 					resource.TestCheckResourceAttr(resourceName, "devices_key_duration_days", "10"),
