a75cbc61d26165e53e66202f9f97d10fb4767379
mcoulombe <max@tailscale.com>
2025-06-13 09:09:44 -0400

    resource_tailscale_oauth_client: add import support
    resource_tailscale_key: handle expiry on imports to avoid systematic recreates
    
    Fixes #515
    
    Signed-off-by: mcoulombe <max@tailscale.com>

---
+++ examples/resources/tailscale_oauth_client/import.sh
@@ -0,0 +1,2 @@
+# Note: Sensitive fields such as the secret key are not returned by the API and will be unset in the Terraform state after import.
+terraform import tailscale_oauth_client.example k1234511CNTRL
