474c87c155c577b410f9c395d6d0ce0785dfa6ec
Alex Chan <alexc@tailscale.com>
2026-04-10 14:03:18 +0100

    resource_dns_split_nameservers: migrate to plugin framework
    Fixes tailscale/corp#37239
    
    Signed-off-by: Alex Chan <alexc@tailscale.com>

--- tailscale/provider_sdk.go
+++ tailscale/provider_sdk.go
@@ -97,7 +97,6 @@ func Provider(options ...ProviderOption) *schema.Provider {
 			"tailscale_dns_configuration":       resourceDNSConfiguration(),
 			"tailscale_dns_nameservers":         resourceDNSNameservers(),
 			"tailscale_dns_search_paths":        resourceDNSSearchPaths(),
-			"tailscale_dns_split_nameservers":   resourceDNSSplitNameservers(),
 			"tailscale_device_authorization":    resourceDeviceAuthorization(),
 			"tailscale_tailnet_key":             resourceTailnetKey(),
 			"tailscale_device_tags":             resourceDeviceTags(),
