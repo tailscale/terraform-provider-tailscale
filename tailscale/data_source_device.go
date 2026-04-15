a1c0ec224339243aa6526df94c15b9a012c0f70c
Gesa Stupperich <gesa@tailscale.com>
2026-04-15 08:02:33 +0100

    data_source_device: port wait_for to plugin framework (#678)
    This adds the retry logic for the device datasource reads which
    had been missed in the initial migration.
    
    Updates tailscale.com/corp#37224
    
    Signed-off-by: Gesa Stupperich <gesa@tailscale.com>

--- tailscale/data_source_device.go
+++ tailscale/data_source_device.go
@@ -5,6 +5,7 @@ package tailscale
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
@@ -206,39 +207,47 @@ func (d deviceDataSource) Read(ctx context.Context, req datasource.ReadRequest, 
 		return
 	}
 
-	var filter func(d tailscale.Device) bool
+	var filter tailscale.ListDevicesOptions
 	var filterDesc string
 
 	if !device.Name.IsNull() {
-		filter = func(d tailscale.Device) bool {
-			return d.Name == device.Name.ValueString()
-		}
+		filter = tailscale.WithFilter("name", []string{device.Name.ValueString()})
 		filterDesc = fmt.Sprintf("name=%q", device.Name.ValueString())
 	}
 
 	if !device.Hostname.IsNull() {
-		filter = func(d tailscale.Device) bool {
-			return d.Hostname == device.Hostname.ValueString()
-		}
-		filterDesc = fmt.Sprintf("name=%q", device.Hostname.ValueString())
+		filter = tailscale.WithFilter("hostname", []string{device.Hostname.ValueString()})
+		filterDesc = fmt.Sprintf("hostname=%q", device.Hostname.ValueString())
 	}
 
-	devices, err := d.Client.Devices().List(ctx)
-	if err != nil {
-		resp.Diagnostics.AddError("Failed to fetch devices", err.Error())
-		return
+	var deadline time.Duration
+	if !device.WaitFor.IsNull() {
+		parsed, err := time.ParseDuration(device.WaitFor.ValueString())
+		if err != nil {
+			resp.Diagnostics.AddError("Failed to parse wait_for", err.Error())
+			return
+		}
+		deadline = parsed
 	}
 
 	var selected *tailscale.Device
-	for _, candidate := range devices {
-		if filter(candidate) {
-			selected = &candidate
-			break
+	poll := func(ctx context.Context) error {
+		devices, err := d.Client.Devices().List(ctx, filter)
+		if err != nil {
+			return err
 		}
+
+		if len(devices) == 0 {
+			return errors.New("could not find device with" + filterDesc)
+		}
+
+		selected = &devices[0]
+		return nil
 	}
 
-	if selected == nil {
-		resp.Diagnostics.AddError("Could not find device", "Could not find device using filter "+filterDesc)
+	err := retryWithDeadline(ctx, poll, deadline, 1*time.Second)
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to fetch devices", err.Error())
 		return
 	}
 
@@ -335,5 +344,39 @@ func deviceToMap(device *tailscale.Device) map[string]any {
 		"update_available":            device.UpdateAvailable,
 		"tailnet_lock_error":          device.TailnetLockError,
 		"tailnet_lock_key":            device.TailnetLockKey,
+	}
+}
+
+// retryWithDeadline calls fn once. If fn errors and maxWait and retryInterval are positive, it retries fn until fn
+// succeeds or maxWait elapses, waiting for the duration of retryInterval between attempts.
+func retryWithDeadline(ctx context.Context, fn func(context.Context) error, maxWait time.Duration, retryInterval time.Duration) error {
+	// Do an initial check in case we don't need to wait at all.
+	err := fn(ctx)
+	if err == nil {
+		return nil
+	}
+	if maxWait <= 0 || retryInterval <= 0 {
+		return err
+	}
+
+	maxTicker := time.NewTicker(maxWait)
+	defer maxTicker.Stop()
+	intervalTicker := time.NewTicker(retryInterval)
+	defer intervalTicker.Stop()
+
+	// Check for the data at intervals, until we reach the maximum specified duration.
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-maxTicker.C:
+			return fmt.Errorf("failed after maximum of retries within %v: %w", maxWait, err)
+		case <-intervalTicker.C:
+			err = fn(ctx)
+			if err != nil {
+				continue
+			}
+			return nil
+		}
 	}
 }
