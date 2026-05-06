99d59a972feefcadb58c2555b8195ab46a240110
Alex Chan <alexc@tailscale.com>
2026-05-06 10:23:08 +0100

    plan_modifiers: add a HuJSON plan modifier
    Updates tailscale/corp#37228
    
    Signed-off-by: Alex Chan <alexc@tailscale.com>

--- tailscale/plan_modifiers.go
+++ tailscale/plan_modifiers.go
@@ -10,10 +10,13 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/tailscale/hujson"
 )
 
 var (
 	_ planmodifier.String = jsonSemanticDiffModifier{}
+	_ planmodifier.String = aclHuJSONModifier{}
 	_ planmodifier.String = PreserveEmptyStringAsNull{}
 )
 
@@ -44,6 +47,40 @@ func (m jsonSemanticDiffModifier) PlanModifyString(ctx context.Context, req plan
 
 	if reflect.DeepEqual(config, state) {
 		resp.PlanValue = req.StateValue
+	}
+}
+
+// aclHuJSONModifier stores strings as their canonical HuJSON representation in
+// the state, and treats strings as equivalent if their canonicalical representation
+// is the same.
+type aclHuJSONModifier struct{}
+
+func (m aclHuJSONModifier) Description(_ context.Context) string {
+	return "Suppresses diffs if two strings have the same canonical HuJSON value."
+}
+
+func (m aclHuJSONModifier) MarkdownDescription(ctx context.Context) string {
+	return m.Description(ctx)
+}
+
+func (m aclHuJSONModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
+	if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
+		return
+	}
+
+	formatted, err := hujson.Format([]byte(req.ConfigValue.ValueString()))
+	if err != nil {
+		return
+	}
+
+	normalizedConfig := string(formatted)
+
+	if !req.StateValue.IsNull() && !req.StateValue.IsUnknown() {
+		stateFormatted, err := hujson.Format([]byte(req.StateValue.ValueString()))
+		if err == nil && string(stateFormatted) == normalizedConfig {
+			resp.PlanValue = req.StateValue
+			return
+		}
 	}
 }
 
