c02ba26d02a65588c11afc989593ba2cff8d9bef
Alex Chan <alexc@tailscale.com>
2026-05-12 12:10:25 +0100

    all: migrate from terraform-plugin-sdk to terraform-plugin-testing
    The testing parts of the plugin SDK have been extracted into a new
    package which has the same API. We need to update the imports and
    a couple of tests where the new package is stricter.
    
    Updates tailscale/corp#37221
    
    Signed-off-by: Alex Chan <alexc@tailscale.com>

--- tailscale/resource_dns_split_nameservers_test.go
+++ tailscale/resource_dns_split_nameservers_test.go
@@ -10,8 +10,8 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 
 	"tailscale.com/client/tailscale/v2"
 )
