be8126fbdc9524784b4575855ad78c2e446fb577
James Sanderson <jsanderson@tailscale.com>
2026-05-14 20:03:44 +0100

    resource_dns_search_paths: treat nil paths as empty
    Currently the API returns an empty list of paths if there are none, the
    field is not omitempty/omitzero, but we should not depend on this detail and
    should be defensive against it.
    
    Updates tailscale/corp#37032
    
    Signed-off-by: James Sanderson <jsanderson@tailscale.com>

--- tailscale/resource_dns_search_paths_test.go
+++ tailscale/resource_dns_search_paths_test.go
@@ -47,6 +47,11 @@ func TestProvider_TailscaleDNSSearchPaths(t *testing.T) {
 func TestAccTailscaleDNSSearchPaths(t *testing.T) {
 	const resourceName = "tailscale_dns_search_paths.test_search_paths"
 
+	const testSearchPathsEmpty = `
+	resource "tailscale_dns_search_paths" "test_search_paths" {
+		search_paths = []
+	}`
+
 	checkProperties := func(expected []string) func(client *tailscale.Client, rs *terraform.ResourceState) error {
 		return func(client *tailscale.Client, rs *terraform.ResourceState) error {
 			actual, err := client.DNS().SearchPaths(context.Background())
@@ -85,6 +90,9 @@ func TestAccTailscaleDNSSearchPaths(t *testing.T) {
 					),
 					resource.TestCheckTypeSetElemAttr(resourceName, "search_paths.*", "example.com"),
 				),
+			},
+			{
+				Config: testSearchPathsEmpty,
 			},
 			{
 				ResourceName:      resourceName,
